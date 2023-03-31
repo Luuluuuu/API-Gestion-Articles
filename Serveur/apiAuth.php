310718c46d75578861f74595b9bde75b4e902ad8
Luuluuuu <lucille4713@gmail.com>
2023-03-31 15:29:09 +0200

    changement code erreur mauvais

--- Serveur/apiAuth.php
+++ Serveur/apiAuth.php
@@ -17,7 +17,7 @@
 
             // Si aucun login ou aucun mdp ne sont renseignés
             if (empty($postedData["login"]) || empty($postedData["mdp"])){
-                deliver_response(400, "Veuillez renseigner votre login et votre mot de passe.", null);
+                deliver_response(404, "Veuillez renseigner votre login et votre mot de passe.", null);
 
             } else {
                 // Récupération des données saisies
